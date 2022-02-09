cb2b96eec4666b68e1d15dd297f26e344ff579f5
Abrar <abrarhabib285@gamil.com>
2022-02-09 18:39:02 -0500

    Added anime.js

--- homepage/script.js
+++ homepage/script.js
@@ -1,3 +1,13 @@
+import anime from 'animejs/lib/anime.es.js'
 
+let welcome = document.querySelector('#welcome')
+
+anime({
+    targets: welcome,
+    translateX: 250,
+    rotate: '1turn',
+    backgroundColor: '#FFF',
+    duration: 800
+  });
 
 
