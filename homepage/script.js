8ec4bfaaf9d512f2eadab6e655d89a5423d2e582
Abrar <abrarhabib285@gamil.com>
2022-02-08 19:52:04 -0500

    More Stuff

--- homepage/script.js
+++ homepage/script.js
@@ -1,23 +1,3 @@
-const para1 = document.getElementById("para1");
-const para2 = document.getElementById("para2");
-const para3 = document.getElementById("para3");
 
-animate(para1);
-animate(para2);
-animate(para3);
-
-function animate(element){
-    let elementWidth = element.elementWidth
-    let parentWidth = element.parentElement.elementWidth;
-    let flag = 0;
-    
-    setInterval(() => {
-        element.style.marginLeft = --flag+ "px";
-        
-        if (elementWidth == -flag) {
-            flag = parentWidth;
-        }
-    }, 10);
-}
 
 
