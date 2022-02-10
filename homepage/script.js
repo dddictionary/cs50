8ca411402cb1199cb22d0723d03e7e46dde9d562
Abrar <abrarhabib285@gamil.com>
2022-02-10 18:16:48 -0500

    Added Homepage Animation

--- homepage/script.js
+++ homepage/script.js
@@ -1,11 +1,35 @@
 import anime from "./node_modules/animejs/lib/anime.es.js"
 
 
-let animation = anime({
-    targets: 'div.welcome',
-    translateX: 100,
-    borderRadius: 50,
+// Wrap every letter in a span
+var textWrapper = document.querySelector('.ml11 .letters');
+textWrapper.innerHTML = textWrapper.textContent.replace(/([^\x00-\x80]|\w)/g, "<span class='letter'>$&</span>");
+
+anime.timeline({loop: true })
+  .add({
+    targets: '.ml11 .line',
+    scaleY: [0,1],
+    opacity: [0.5,1],
+    easing: "easeOutExpo",
+    duration: 700
+  })
+  .add({
+    targets: '.ml11 .line',
+    translateX: [0, document.querySelector('.ml11 .letters').getBoundingClientRect().width + 10],
+    easing: "easeOutExpo",
+    duration: 700,
+    delay: 100
+  }).add({
+    targets: '.ml11 .letter',
+    opacity: [0,1],
+    easing: "easeOutExpo",
+    duration: 600,
+    offset: '-=775',
+    delay: (el, i) => 34 * (i+1)
+  }).add({
+    targets: '.ml11',
+    opacity: 0,
     duration: 1000,
-    easing: 'linear',
-    direction: 'alternate'
+    easing: "easeOutExpo",
+    delay: 1000
   });
